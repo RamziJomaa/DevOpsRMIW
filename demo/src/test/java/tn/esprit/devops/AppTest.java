e68bc76291a6d06e6177fe9f6d23a9272f0b0860
RamziJomaa <ramzi.jomaa@esprit.tn>
2025-11-16 15:39:35 +0100

    Add simple Maven project for Jenkins pipeline test

---
+++ demo/src/test/java/tn/esprit/devops/AppTest.java
@@ -0,0 +1,13 @@
+package tn.esprit.devops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true, "Le test passe toujours !");
+    }
+}
+
